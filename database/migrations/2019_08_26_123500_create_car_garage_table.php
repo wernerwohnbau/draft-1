b7bcc0bc6b7c29406c1c88542bd50f5e495809c5
wernerwohnbau <48991109+wernerwohnbau@users.noreply.github.com>
2019-08-26 12:35:46 +0200

    Pipedream project files

---
+++ database/migrations/2019_08_26_123500_create_car_garage_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarGarageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_garage', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->unsignedBigInteger('car_id')->nullable();
+            $table->foreign('car_id')->references('id')->on('cars');
+            $table->unsignedBigInteger('garage_id')->nullable();
+            $table->foreign('garage_id')->references('id')->on('garages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_garage');
+    }
+}
